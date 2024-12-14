9f815fe25fc944cd15fc9a4e4a8ed870c06df981
Tony Lin <tonyelin@gmail.com>
2024-12-14 11:36:26 +0800

    update Changelog.py

--- py50-streamlit/page/Changelog.py
+++ py50-streamlit/page/Changelog.py
@@ -21,9 +21,21 @@ CHANGELOG BELOW
 
 st.markdown('# :red[**Changelog**]')
 
-st.markdown('## :rainbow[**2024.05.17**]')
+st.markdown('## :rainbow[**2024.12.14**]')
 st.markdown('''
 ##### :green[**Major Changes**] 🎉
+py50-streamlit has been updated to [py50 v1.0.9](https://github.com/tlint101/py50/releases)!
+
+Bugs have been fixed for box and violin plots! 
+            ''')
+
+st.markdown('''  
+<br><br>
+:red[2024.05.17]
+
+''', unsafe_allow_html=True)
+st.markdown('''
+:green[**Major Changes**] 🎉
 py50-streamlit has been updated to [py50 v1.0.6](https://github.com/tlint101/py50/releases)!
 
 Users can perform basic statistical tests and generate annotated plots!
